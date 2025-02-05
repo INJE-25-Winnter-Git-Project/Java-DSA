5aedc3fd312eddd7fe16ad25ce82242ef6e890cd
DESKTOP-E2QAJHS\혜원 <hewon0933@gmail.com>
2025-02-05 09:32:25 +0900

    상수
    1. 상수란? 수식에서 변하지 않는 값
    
    2. 상수의 선언
      - 앞에 final을 붙임
      - 상수는 대문자의 명명규칙을 사용
      - 여러 단어로 구성된 경우 '_' 사용
      cf) 변수의 명명 규칙
       1) 첫 번째 문자가 소문자인 명사로 정함
       2) 여러 단어로 구성된 이름의 경우 두번째 단어부터 첫글자를 대문자로 함
       3) '_'을 쓰지 않음
    
      ex) final int J;
          상수명 = 값;
           J = 10; (J에는 값을 담을 수 있는 기회가 단 한 번만 허락됨)
    
    3. 상수를 사용해야 하는 경우
       - 값이 변하면 위험한 경우에 상수를 사용
       - 값만 봤을때 무엇을 의미하는지 쉽게 파악할 수 없는 값에도 값 자체를 사용하기 보다는 상수를 사용

---
+++ Java-DSA/src/java/constant.java
@@ -0,0 +1,29 @@
+package java;
+
+public class constant {
+    public static void main(String[] args) {
+        int i;
+        i = 10;
+        i = 5;
+
+        final int J;
+        J = 10;
+
+//        J = 5;
+
+        double circleArea;
+        final double PI = 3.14159;
+        circleArea = 3 * 3 * PI;
+
+        final int OIL_PRICE = 1450;
+
+        int totalPrice = 50 * OIL_PRICE;
+
+//        예제
+        final double PIE = 3.14159;
+
+        int radius = 5;
+        double result = radius * 2 * PIE;
+        System.out.println(result);
+    }
+}
