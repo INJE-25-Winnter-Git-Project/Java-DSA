acc0b60469820d4075f347d369cb50e49aeb3783
DESKTOP-E2QAJHS\혜원 <hewon0933@gmail.com>
2025-02-07 10:08:46 +0900

    while문 & do while문
    1. 반복문
    - 어떠한 것을 반복적으로 사용하고 싶을 때
    - 자바에서는 반복문으로 while, do while, for문을 제공
    
    2. do while문
    - while문의 경우 조건이 만족되지 않을 때 실행되지 않음
    - do while문은 무조건 한 번은 수행 됨

---
+++ Java-DSA/src/java/While.java
@@ -0,0 +1,38 @@
+package java;
+
+import Java.util.Scanner;
+
+public class While {
+    public static void main(String[] args) {
+//        int i = 0;
+//
+//        while(i<10) {
+//            System.out.println(i);
+//            i++; // i=i+1;
+//        }
+
+//        int total = 0;
+//        int i = 1;
+//        while(i<=100) {
+//            total = total + i;
+//            i++;
+//        }
+//        System.out.println(total);
+//
+//        while(true) {
+//            System.out.println("hello");
+//        }
+
+        // do while
+        int value = 0;
+        scanner can = new Scanner(system.in);
+
+        do{
+            //반복할 문장들
+            vaule = scan.nextInt();
+            System.out.println("입력 받은 값 : " +value);
+        }while(value != 10);
+
+        System.out.println("반복문 종료!");
+    }
+}
